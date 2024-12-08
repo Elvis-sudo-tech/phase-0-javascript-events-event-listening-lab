eab4a9554a3b620b27d7873f8ba2ae2bc2d01d3b
Elvis-Tech-spec <manfuturekahande@gmail.com>
2024-12-09 01:03:59 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button'); // Update this to match the 'button' ID
+    if (button) {
+      button.addEventListener('click', () => {
+        alert('I was clicked!');
+      });
+    } else {
+      console.error("Element with id 'button' not found.");
+    }
+  }
+  
